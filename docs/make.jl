783e407cbb067458db9d36a8f25203d053bd7b08
Syver Døving Agdestein <syverda@gmail.com>
2025-01-20 21:17:02 +0100

    chore: format files

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,18 @@
 using Turbulox
 using Documenter
 
-DocMeta.setdocmeta!(Turbulox, :DocTestSetup, :(using Turbulox); recursive=true)
+DocMeta.setdocmeta!(Turbulox, :DocTestSetup, :(using Turbulox); recursive = true)
 
 makedocs(;
-    modules=[Turbulox],
-    authors="Syver Døving Agdestein <syverda@gmail.com> and contributors",
-    sitename="Turbulox.jl",
-    format=Documenter.HTML(;
-        canonical="https://agdestein.github.io/Turbulox.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [Turbulox],
+    authors = "Syver Døving Agdestein <syverda@gmail.com> and contributors",
+    sitename = "Turbulox.jl",
+    format = Documenter.HTML(;
+        canonical = "https://agdestein.github.io/Turbulox.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/agdestein/Turbulox.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/agdestein/Turbulox.jl", devbranch = "main")
