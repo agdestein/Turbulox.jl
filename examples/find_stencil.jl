83836abc877895ef3480b7f3af7e1d51058b2c6c
Syver Døving Agdestein <syverda@gmail.com>
2025-01-23 21:46:22 +0100

    feat: add automatic stencil finders

--- examples/find_stencil.jl
+++ examples/find_stencil.jl
@@ -1,12 +1,31 @@
-stencil = [-9, 125, -2250, 2250, -125, 9]
-stencil = vcat(stencil, fill(0, length(stencil)))
+# Find coefficients for linear combination of δ1, δ3, δ5, etc
+# such that the leading order error terms cancel out
+# Note: order = 2n
+function find_stencil(n)
+    A = broadcast((i, j) -> (2j - 1)^2i // 1, 1:n-1, (1:n)') # Note: Matrix of rationals
+    A = vcat(A, fill(1, 1, n)) # Last row: ones (sum of coefficients)
+    y = map(i -> i == n, 1:n) # Right-hand side: zeros (terms cancel out), except for last element (sum of coefficients = 1)
+    x = A \ y # Solve for coefficients x
+end
 
-rows = map(i -> circshift(stencil, i-1), eachindex(stencil))
+# Make circular matrix for d/dx, then square it to
+# get laplacian stencil d^2/dx^2
+function find_laplacian(weights)
+    weights = map(i -> weights[i] / (2i - 1), eachindex(weights)) # Account for coarser grid sizes of δ3, δ5, etc.
+    stencil = vcat(-reverse(weights), weights) # Make symmetric ("right minus left")
+    stencil = vcat(stencil, fill(0, length(stencil) - 1)) # Pad with zeros so we get all combinations
+    rows = map(i -> circshift(stencil, i - 1), eachindex(stencil)) # Make circular matrix
+    mat = stack(rows)' # Assemble matrix for d/dx
+    a = (mat * mat) # Square to get laplacian stencil
+    a[1, :] # Take stencil from first row
+end
 
-mat = stack(rows)'
-a = (mat * mat)[1, :]
+find_stencil(1) # Second order
+find_stencil(2) # Fourth order
+find_stencil(3) # Sixth order
+find_stencil(4) # Eight order
 
-
-a // 1920^2
-
-1920^2
+find_laplacian(find_stencil(1))
+find_laplacian(find_stencil(2))
+find_laplacian(find_stencil(3))
+find_laplacian(find_stencil(4))
