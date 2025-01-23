a8212a84813789ff271c5c16c4e1e4937d2d66ea
Syver Døving Agdestein <syverda@gmail.com>
2025-01-23 19:40:41 +0100

    fix: initializers

--- src/initializers.jl
+++ src/initializers.jl
@@ -13,11 +13,19 @@ vectorfield(setup) = KernelAbstractions.zeros(
     dim(setup.grid),
 )
 
-"Allocate empty tensor field."
-function tensorfield(setup)
+"Allocate empty tensor field (collocated)."
+function collocated_tensorfield(setup)
     (; backend, grid, visc) = setup
     d = dim(grid)
     d2 = d * d
     T = typeof(visc)
-    KernelAbstractions.zeros(backend, SMatrix{d,d,T,d2}, ntuple(Returns(grid.n), D))
+    KernelAbstractions.zeros(backend, SMatrix{d,d,T,d2}, ntuple(Returns(grid.n), d))
+end
+
+"Allocate empty tensor field (staggered)."
+function staggered_tensorfield(setup)
+    (; backend, grid, visc) = setup
+    d = dim(grid)
+    T = typeof(visc)
+    KernelAbstractions.zeros(backend, T, ntuple(Returns(grid.n), d)..., d, d)
 end
