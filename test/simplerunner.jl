135d2f5666dfb33375802f0e014e1ed7d9be00a6
Syver Døving Agdestein <syverda@gmail.com>
2025-01-26 14:18:43 +0100

    feat: add live runner for testitems

---
+++ test/simplerunner.jl
@@ -0,0 +1,20 @@
+# Including this file makes `@testitem`s executable in the REPL.
+# Each testitem is wrapped in a module to avoid name conflicts.
+
+module TestRunner
+    macro testitem(name, block)
+        quote
+            @eval module MyTest
+                using Turbulox
+                using Test
+                @testset $name begin
+                    $block
+                end
+            end
+            nothing # Suppress output
+        end
+    end
+end
+
+using REPL
+REPL.activate(TestRunner)
